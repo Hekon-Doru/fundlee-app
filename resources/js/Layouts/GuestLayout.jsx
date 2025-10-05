b04a7df7cee7c0aeec7dbb36d844f39d3cb932e1
Hekon-Doru <vytenis.kazlas@gmail.com>
2025-10-05 22:45:42 +0300

    Updated | GuestLayout style

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,14 +1,42 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import { Link } from "@inertiajs/react";
 
 export default function GuestLayout({ children }) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
-                </Link>
-            </div>
+        <div className="bg-gray-50 dark:bg-gray-900 min-h-screen flex flex-col">
+            <header className="flex justify-between items-center py-6 px-6 md:px-12 bg-white dark:bg-gray-800 shadow-md">
+                    <Link
+                        href={route("story.list")}
+                        className="px-4 py-2 rounded-md text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 transition"
+                        >
+                <div className="text-2xl font-bold text-green-500 dark:text-white">
+                        Fundlee
+                </div>
+                        {" "}
+                    </Link>{" "}
+                <nav className="flex gap-4">
+                    <>
+                        <Link
+                            href={route("story.list")}
+                            className="px-4 py-2 rounded-md text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 transition"
+                        >
+                            Stories
+                        </Link>
+                        <Link
+                            href={route("login")}
+                            className="px-4 py-2 rounded-md text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 transition"
+                        >
+                            Log In
+                        </Link>
+                        <Link
+                            href={route("register")}
+                            className="px-4 py-2 rounded-md text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 transition"
+                        >
+                            Register
+                        </Link>
+                    </>
+                </nav>
+            </header>
 
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 {children}
